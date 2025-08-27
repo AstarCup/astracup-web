2505260fbf79b6aa30959d9510bfb90630307d83
AeCw <aecbanana@outlook.com>
2025-08-27 22:13:21 +0800

    add speedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import localFont from "next/font/local";
 
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${alimama.className} ${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         <Analytics />
+        <SpeedInsights />
         <main className={`pt-16`}>
           {children}
         </main>
